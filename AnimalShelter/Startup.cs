c3db7ce068863b401298d278a0fc97d07eed9e79
Amber W <amoothenielson@gmail.com>
2022-04-21 17:33:25 -0700

    Add Swagger to startup code.

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -69,6 +69,7 @@ namespace AnimalShelter
                     app.UseSwaggerUI(c => 
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelterApi v1");
+                    c.RoutePrefix = string.Empty;
                 });
             }
 
